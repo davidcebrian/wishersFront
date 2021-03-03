45efbee9d2de467b4741d195084ad038c3211f8e
David <davidcebriancortes0@gmail.com>
2021-03-03 11:58:18 +0100

    anadiendo scss bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/static/home/home.component';
+import { NavbarComponent } from './components/static/navbar/navbar.component';
+import { FooterComponent } from './components/static/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
